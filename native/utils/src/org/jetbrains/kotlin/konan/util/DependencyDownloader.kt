fc797d45ceb658583f1da161d2490a0f0a222958
Anton Saatze <anton@MBP-von-Anton.fritz.box>
2023-11-23 08:10:22 +0100

    update package.py and build llvm stable with version stable/20230725. command: python3 package.py --branch stable/20230725. Zip created folder and put in ~/.konan/dependencies/cache. Update DependencyDownloader to not crash when loading local file.

--- native/utils/src/org/jetbrains/kotlin/konan/util/DependencyDownloader.kt
+++ native/utils/src/org/jetbrains/kotlin/konan/util/DependencyDownloader.kt
@@ -145,7 +145,10 @@ class DependencyDownloader(
 
     /** Performs an attempt to download a specified file into the specified location */
     private fun tryDownload(url: URL, tmpFile: File) {
-        if (url.file.contains("riscv64-lp64d--glibc--stable-2021") || url.file.contains("aarch64--glibc--stable-2023.08-1")) {
+        if (url.file.contains("riscv64-lp64d--glibc--stable-2021") ||
+            url.file.contains("aarch64--glibc--stable-2023.08-1") ||
+            url.file.contains("apple-llvm-20230725-macos-x64-1")
+            ) {
             println("tryDownload(): ignore $url to $tmpFile")
             return
         }
